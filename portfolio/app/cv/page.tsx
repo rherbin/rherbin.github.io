4da29c944a3ca68b9e92cbd7a7bb8667fdb31e7b
Rodolphe Herbin <84749107+rherbin@users.noreply.github.com>
2024-03-12 10:54:16 +0100

    Continued building CV

--- portfolio/app/cv/page.tsx
+++ portfolio/app/cv/page.tsx
@@ -1,10 +1,11 @@
 import Image from 'next/image'
 import { Separator } from '@/components/ui/separator'
+import { Progress } from '@/components/ui/progress'
 
 export default function Cv(){
     return (
-        <div className="grid grid-cols-5">
-            <div className="col-start-2 col-span-3 border-2 rounded-xl justify-center">
+        <div className="flex justify-center my-5">
+            <div className="border-2 rounded-xl">
 
                 <div className="flex items-center justify-center">
                     <Image className="rounded-xl border-2 m-4 mr-auto" width={200} height={300} src="/me.png"></Image>
@@ -13,7 +14,9 @@ export default function Cv(){
                     </div>
                 </div>
 
-                <div className='flex h-5 space-x-4 items-center text-base m-4 justify-center text-gray-400'>
+                <Separator className="mx-10 mt-5 mb-8 w-auto"></Separator>
+
+                <div className='flex h-10 space-x-4 items-center text-base m-4 justify-center text-gray-400'>
                     <div>E-mail : rodolpherbin@gmail.com</div>
                     <Separator orientation='vertical' className="m-2"></Separator>
                     <div>Github : <a href="https://github.com/rherbin">rherbin</a></div>
@@ -21,36 +24,74 @@ export default function Cv(){
                     <div>Phone : +33 6 43 51 72 05</div>
                 </div>
 
-                <Separator className="mx-10 mt-8 mb-5 w-auto"></Separator>
+                <Separator className="mx-10 mt-8 mb-2 w-auto"></Separator>
                 
-                <div className='flex m-10 space-x-4 w-full'>
-                    <div className='w-80'>
+                <div className='flex space-x-4 w-full'>
+                    <div className='w-80 m-5'>
                         <h2 className='text-2xl m-5 font-bold'>Compétences</h2>
-                        <div>Python</div>
-                        <div>C/C++</div>
-                        <div>Javascript</div>
-                        <div>Haskell</div>
+
+                        <div className='rounded-xl border-2 my-5 p-4'>
+                            <h3 className='m-2 font-semibold text-xl'>Programmation</h3>
+                            <div className='p-2'>
+                                <div>Python</div><Progress value="90" className="w-[100%]"></Progress>
+                            </div>
+                            <div className='p-2'>
+                                <div>C/C++</div><Progress value="70" className="w-[100%]"></Progress>
+                            </div>
+                            <div className='p-2'>
+                                <div>Javascript/Typescript</div><Progress value="50" className="w-[100%]"></Progress>
+                            </div>
+                            <div className='p-2'>
+                                <div>Haskell</div><Progress value="30" className="w-[100%]"></Progress>
+                            </div>
+                            <div className='p-2'>
+                                <div>Rust</div><Progress value="10" className="w-[100%]"></Progress>
+                            </div>
+                        </div>
+                        
+                        <div className='rounded-xl border-2 my-5 p-4'>
+                            <h3 className='m-2 font-semibold text-xl'>Langues</h3>
+                            <div className='p-2'>
+                                <div>Français</div><Progress value="100" className="w-[100%]"></Progress>
+                            </div>
+                            <div className='p-2'>
+                                <div>Anglais</div><Progress value="80" className="w-[100%]"></Progress>
+                            </div>
+                            <div className='p-2'>
+                                <div>Allemand</div><Progress value="50" className="w-[100%]"></Progress>
+                            </div>
+                        </div>
                     </div>
-                    <Separator orientation="vertical" className="h-auto">
 
-                    </Separator>
-                    <div className=''>
-                        <h2 className='text-2xl m-5 font-bold'>Formation</h2>
+                    <Separator orientation="vertical" className="h-auto"></Separator>
 
-                        <div className='rounded-xl border-2 m-2 p-6'>
-                            <div className='font-bold'>2015 - 2019</div><div>Collège Claude Debussy, Paris 75015</div><div>Diplôme : Diplôme National du Brevet des Collèges</div>
+                    <div className='m-5 pl-3'>
+                        
+                        <div className='my-5'>
+                            <h2 className='text-2xl m-5 font-bold'>Formation</h2>
+
+                            <div className='rounded-xl border-2 m-2 mr-5 p-6'>
+                                <div className='font-bold'>2015 - 2019</div><div>Collège Claude Debussy, Paris 75015 | Diplôme : Brevet des Collèges</div>
+                            </div>
+
+                            <div className='rounded-xl border-2 m-2 mr-5 p-6'>
+                                <div className='font-bold'>2019 - 2021</div><div>Lycée Jean Baptiste Say, Paris 75016</div><div>Spécialités : Mathématiques - Physique Chimie - NSI</div>
+                            </div>
+
+                            <div className='rounded-xl border-2 m-2 mr-5 p-6'>
+                                <div className='font-bold'>2021 - 2022</div><div>Lycée Victor Duruy, Paris 75007 | Diplôme : Baccalauréat</div><div>Spécialités : Mathématiques - NSI</div>
+                            </div>
+
+                            <div className='rounded-xl border-2 m-2 mr-5 p-6'>
+                                <div className='font-bold'>Depuis 2022</div><div>Université Paris Dauphine, Paris 75116</div><div>Filière : MIDO Mathématiques Informatique</div>
+                            </div>
                         </div>
 
-                        <div className='rounded-xl border-2 m-2 p-6'>
-                            <div className='font-bold'>2019 - 2021</div><div>Lycée Jean Baptiste Say, Paris 75016</div><div>Spécialités : Mathématiques - Physique Chimie - NSI</div><div>Diplôme : Baccalauréat</div>
-                        </div>
-
-                        <div className='rounded-xl border-2 m-2 p-6'>
-                            <div className='font-bold'>2021 - 2022</div><div>Lycée Victor Duruy, Paris 75007</div><div>Spécialités : Mathématiques - NSI</div>
-                        </div>
-
-                        <div className='rounded-xl border-2 m-2 p-6'>
-                            <div className='font-bold'>Depuis 2022</div><div>Université Paris Dauphine, Paris 75116</div><div>Filière : MIDO Mathématiques Informatique</div>
+                        <div className='my-5'>
+                            <h2 className='text-2xl m-5 font-bold'>Expérience</h2>
+                            <div className='rounded-xl border-2 m-2 mr-5 p-6'>
+                                <div className='font-bold'>2019 - Stage</div><div>Office National d'Études et de Recherches Aérospatiales</div>
+                            </div>
                         </div>
 
                     </div>
