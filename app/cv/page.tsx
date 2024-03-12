eaf0cc728545b3370a7f8b7cc6fcb568b42c3312
Rodolphe Herbin <84749107+rherbin@users.noreply.github.com>
2024-03-12 12:13:20 +0100

    Reorganized files

--- app/cv/page.tsx
+++ app/cv/page.tsx
@@ -8,9 +8,10 @@ export default function Cv(){
             <div className="border-2 rounded-xl">
 
                 <div className="flex items-center justify-center">
-                    <Image className="rounded-xl border-2 m-4 mr-auto" width={200} height={300} src="/me.png"></Image>
+                    <Image alt="Profile picture" className="rounded-xl border-2 m-4 mr-auto" width={200} height={300} src="/me.png"></Image>
                     <div className='text-center mr-auto'>
                         <h1 className="m-4 font-bold text-5xl">Rodolphe Herbin</h1>
+                        <h2 className='m-2 font-semibold text-3xl text-gray-400'>Étudiant</h2>
                     </div>
                 </div>
 
@@ -19,9 +20,11 @@ export default function Cv(){
                 <div className='flex h-10 space-x-4 items-center text-base m-4 justify-center text-gray-400'>
                     <div>E-mail : rodolpherbin@gmail.com</div>
                     <Separator orientation='vertical' className="m-2"></Separator>
-                    <div>Github : <a href="https://github.com/rherbin">rherbin</a></div>
+                    <div>Github : <a href="https://github.com/rherbin" target="_blank" className='underline underline-offset-2 decoration-4 hover:text-black hover:bg-gray-400 hover:decoration-slate-400'>rherbin</a></div>
                     <Separator orientation='vertical' className="m-2"></Separator>
                     <div>Phone : +33 6 43 51 72 05</div>
+                    <Separator orientation='vertical' className="m-2"></Separator>
+                    <div>Site : <a href="https://rherbin.github.io/" target='_blank' className='underline underline-offset-2 decoration-4 hover:text-black hover:bg-gray-400 hover:decoration-slate-400'>rherbin.github.io</a></div>
                 </div>
 
                 <Separator className="mx-10 mt-8 mb-2 w-auto"></Separator>
@@ -33,37 +36,47 @@ export default function Cv(){
                         <div className='rounded-xl border-2 my-5 p-4'>
                             <h3 className='m-2 font-semibold text-xl'>Programmation</h3>
                             <div className='p-2'>
-                                <div>Python</div><Progress value="90" className="w-[100%]"></Progress>
+                                <div>Python</div><Progress value={90} className="w-[100%]"></Progress>
                             </div>
                             <div className='p-2'>
-                                <div>C/C++</div><Progress value="70" className="w-[100%]"></Progress>
+                                <div>C/C++</div><Progress value={70} className="w-[100%]"></Progress>
                             </div>
                             <div className='p-2'>
-                                <div>Javascript/Typescript</div><Progress value="50" className="w-[100%]"></Progress>
+                                <div>Javascript/Typescript</div><Progress value={50} className="w-[100%]"></Progress>
                             </div>
                             <div className='p-2'>
-                                <div>Haskell</div><Progress value="30" className="w-[100%]"></Progress>
+                                <div>Haskell</div><Progress value={30} className="w-[100%]"></Progress>
                             </div>
                             <div className='p-2'>
-                                <div>Rust</div><Progress value="10" className="w-[100%]"></Progress>
+                                <div>Rust</div><Progress value={10} className="w-[100%]"></Progress>
                             </div>
                         </div>
                         
                         <div className='rounded-xl border-2 my-5 p-4'>
                             <h3 className='m-2 font-semibold text-xl'>Langues</h3>
                             <div className='p-2'>
-                                <div>Français</div><Progress value="100" className="w-[100%]"></Progress>
+                                <div>Français</div><Progress value={100} className="w-[100%]"></Progress>
                             </div>
                             <div className='p-2'>
-                                <div>Anglais</div><Progress value="80" className="w-[100%]"></Progress>
+                                <div>Anglais</div><Progress value={80} className="w-[100%]"></Progress>
                             </div>
                             <div className='p-2'>
-                                <div>Allemand</div><Progress value="50" className="w-[100%]"></Progress>
+                                <div>Allemand</div><Progress value={50} className="w-[100%]"></Progress>
                             </div>
+                        </div>
+
+                        <div className='rounded-xl border-2 my-5 p-4'>
+                            <h3 className='m-2 font-semibold text-xl'>Passions</h3>
+                            <ul className='p-4 list-disc'>
+                                <li className='p-1'>Informatique</li>
+                                <li className='p-1'>Jeux-videos</li>
+                                <li className='p-1'>Cinéma</li>
+                                <li className='p-1'>Finance</li>
+                            </ul>
                         </div>
                     </div>
 
-                    <Separator orientation="vertical" className="h-auto"></Separator>
+                    <Separator orientation="vertical" className="h-auto mb-2"></Separator>
 
                     <div className='m-5 pl-3'>
                         
@@ -83,7 +96,7 @@ export default function Cv(){
                             </div>
 
                             <div className='rounded-xl border-2 m-2 mr-5 p-6'>
-                                <div className='font-bold'>Depuis 2022</div><div>Université Paris Dauphine, Paris 75116</div><div>Filière : MIDO Mathématiques Informatique</div>
+                                <div className='font-bold'>Depuis 2022</div><div>Université Paris Dauphine, Paris 75116</div><div>License Mathématiques Informatique</div>
                             </div>
                         </div>
 
