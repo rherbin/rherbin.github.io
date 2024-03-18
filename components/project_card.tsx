d6e18ce301f619e2d39d0d478f1a7733c0d9872f
Rodolphe Herbin <84749107+rherbin@users.noreply.github.com>
2024-03-18 22:05:06 +0100

    Changed resume image and added link to projects descriptions

--- components/project_card.tsx
+++ components/project_card.tsx
@@ -5,8 +5,12 @@ import { Separator } from "./ui/separator"
 
 export function ProjectCard({prurl}:{prurl:string}){
     const imgurl = prurl.replace("github", "raw.githubusercontent")+"/main/cover_image.png";
+    const txt = fetch(prurl.replace("github", "raw.githubusercontent")+"/main/desc.txt")
+        .then((response) => {
+            return response.text()
+        });
     const prname = prurl.split("/").slice(-1);
-    const prlang = fetch(prurl.replace("github.com", "api.github.com/repos"))
+    // const prlang = fetch(prurl.replace("github.com", "api.github.com/repos"))
     return (
         <div className="rounded-xl border-2 w-96 m-2 overflow-hidden">
             <Image src={imgurl} alt="Project image" className="object-cover h-40 m-0 p-0" unoptimized width={384} height={150}></Image>
@@ -14,8 +18,8 @@ export function ProjectCard({prurl}:{prurl:string}){
             <div className="m-4">
                 <h1 className="text-xl font-bold m-4">{prname}</h1>
                 <div className="text-sm text-gray-300">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident labore a voluptatibus odio quasi quia quidem quisquam possimus, dolore sed nobis pariatur ratione, ad omnis ab aut? Fugit, delectus harum? (WIP)
-                    <a href={prurl} className="mx-2 underline underline-offset-2 decoration-4 hover:text-black hover:bg-gray-400 hover:decoration-slate-400 duration-100">View</a>
+                    {txt}
+                    <a href={prurl} className="mx-1 underline underline-offset-2 decoration-4 hover:text-black hover:bg-gray-400 hover:decoration-slate-400 duration-100">View</a>
                 </div>
             </div>
         </div>
