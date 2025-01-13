f7bc21f0c4e5c71a861ee8c8e15930e58ec43b60
Rodolphe Herbin <84749107+rherbin@users.noreply.github.com>
2025-01-13 19:23:41 +0100

    New english resume

--- app/cven/page.tsx
+++ app/cven/page.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image"
 import { Separator } from "@/components/ui/separator"
 import { Progress } from "@/components/ui/progress"
+import Link from 'next/link'
+
 
 export default function Cv(){
     return (
         <div>
+            <Link href="/cvfr" className="absolute left-3 top-3 border-2 rounded-xl p-3">Français</Link>
             <div className="fixed left-0 top-0 -z-10 h-full w-full">
                 <div className="relative h-full w-full bg-slate-950"><div className="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div><div className="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div></div>
             </div>
@@ -15,7 +18,7 @@ export default function Cv(){
                         <Image alt="Profile picture" className="rounded-xl border-2 m-4 mr-auto" unoptimized width={200} height={300} src="/20240902_165937566_iOS.png"></Image>
                         <div className="text-center mr-auto">
                             <h1 className="m-4 font-bold text-5xl">Rodolphe Herbin</h1>
-                            <h2 className="m-2 font-semibold text-3xl text-gray-400">Étudiant</h2>
+                            <h2 className="m-2 font-semibold text-3xl text-gray-400">Student</h2>
                         </div>
                     </div>
 
@@ -28,17 +31,17 @@ export default function Cv(){
                         <Separator orientation="vertical" className="m-2"></Separator>
                         <div>Phone : +33 6 43 51 72 05</div>
                         <Separator orientation="vertical" className="m-2"></Separator>
-                        <div>Site : <a href="https://rherbin.github.io/" target="_blank" className="underline underline-offset-2 decoration-4 hover:text-black hover:bg-gray-400 hover:decoration-slate-400 duration-100">rherbin.github.io</a></div>
+                        <div>Website : <a href="https://rherbin.github.io/" target="_blank" className="underline underline-offset-2 decoration-4 hover:text-black hover:bg-gray-400 hover:decoration-slate-400 duration-100">rherbin.github.io</a></div>
                     </div>
 
                     <Separator className="mx-10 mt-8 mb-2 w-auto"></Separator>
                     
                     <div className="flex space-x-4 w-full">
                         <div className="w-80 m-5">
-                            <h2 className="text-2xl m-5 font-bold">Compétences</h2>
+                            <h2 className="text-2xl m-5 font-bold">Skills</h2>
 
                             <div className="rounded-xl border-2 my-5 p-4">
-                                <h3 className="m-2 font-semibold text-xl">Programmation</h3>
+                                <h3 className="m-2 font-semibold text-xl">Coding</h3>
                                 <div className="p-2">
                                     <div>Python</div><Progress value={95} className="w-[100%]"></Progress>
                                 </div>
@@ -57,26 +60,26 @@ export default function Cv(){
                             </div>
                             
                             <div className="rounded-xl border-2 my-5 p-4">
-                                <h3 className="m-2 font-semibold text-xl">Langues</h3>
+                                <h3 className="m-2 font-semibold text-xl">Languages</h3>
                                 <div className="p-2">
-                                    <div>Français</div><Progress value={100} className="w-[100%]"></Progress>
+                                    <div>French</div><Progress value={100} className="w-[100%]"></Progress>
                                 </div>
                                 <div className="p-2">
-                                    <div>Anglais</div><Progress value={80} className="w-[100%]"></Progress>
+                                    <div>English</div><Progress value={80} className="w-[100%]"></Progress>
                                 </div>
                                 <div className="p-2">
-                                    <div>Allemand</div><Progress value={50} className="w-[100%]"></Progress>
+                                    <div>German</div><Progress value={50} className="w-[100%]"></Progress>
                                 </div>
                             </div>
 
                             <div className="rounded-xl border-2 my-5 p-4">
                                 <h3 className="m-2 font-semibold text-xl">Passions</h3>
                                 <ul className="p-4 list-disc">
-                                    <li className="p-1">Informatique</li>
-                                    <li className="p-1">Jeux vidéo</li>
-                                    <li className="p-1">Cinéma</li>
+                                    <li className="p-1">Computer science</li>
+                                    <li className="p-1">Video games</li>
+                                    <li className="p-1">Cinema</li>
                                     <li className="p-1">Finance</li>
-                                    <li className="p-1">Musique</li>
+                                    <li className="p-1">Music</li>
                                 </ul>
                             </div>
                         </div>
@@ -86,34 +89,34 @@ export default function Cv(){
                         <div className="m-5 pl-3">
                             
                             <div className="my-5">
-                                <h2 className="text-2xl m-5 font-bold">Formation</h2>
+                                <h2 className="text-2xl m-5 font-bold">Education</h2>
 
                                 <div className="rounded-xl border-2 m-2 mr-5 p-6">
-                                    <div className="font-bold mb-1">Depuis 2022</div><div>Université Paris Dauphine, Paris 75116</div><div>Licence Mathématiques Informatique, filière IM2D</div>
+                                    <div className="font-bold mb-1">Since 2022</div><div>Paris Dauphine PSL University, Paris 75116</div><div>Bachelor in Mathematics and Computer Science, IM2D track</div>
                                 </div>
 
                                 <div className="rounded-xl border-2 m-2 mr-5 p-6">
-                                    <div className="font-bold mb-1">2021 - 2022</div><div>Lycée Victor Duruy, Paris 75007 | Diplôme : Baccalauréat</div><div>Spécialités : Mathématiques - NSI</div>
+                                    <div className="font-bold mb-1">2021 - 2022</div><div>Victor Duruy Highschool, Paris 75007 | Diploma : Baccalauréat</div><div>Studied : Mathematics - Computer science</div>
                                 </div>
 
                                 <div className="rounded-xl border-2 m-2 mr-5 p-6">
-                                    <div className="font-bold mb-1">2019 - 2021</div><div>Lycée Jean Baptiste Say, Paris 75016</div><div>Spécialités : Mathématiques - Physique Chimie - NSI</div>
+                                    <div className="font-bold mb-1">2019 - 2021</div><div>Jean Baptiste Say Highschool, Paris 75016</div><div>Studied : Mathematics - Physics - Computer science</div>
                                 </div>
 
                                 <div className="rounded-xl border-2 m-2 mr-5 p-6">
-                                    <div className="font-bold mb-1">2015 - 2019</div><div>Collège Claude Debussy, Paris 75015 | Diplôme : Brevet des Collèges</div>
+                                    <div className="font-bold mb-1">2015 - 2019</div><div>Claude Debussy Middle School, Paris 75015 | Diploma : Brevet des Collèges</div>
                                 </div>
                             </div>
 
                             <Separator className="w-auto mr-8 mt-8"></Separator>                        
 
                             <div className="my-5">
-                                <h2 className="text-2xl m-5 font-bold">Expérience</h2>
+                                <h2 className="text-2xl m-5 font-bold">Experience</h2>
                                 <div className="rounded-xl border-2 m-2 mr-5 p-6">
-                                    <div className="font-bold mb-1">Mai 2023 - Juin 2023</div><div>Pharmacie Matignon - Assistant en Pharmacie</div><div>Logistique & Réorganisation des Rayons</div>
+                                    <div className="font-bold mb-1">June 2023 - July 2023</div><div>Matignon Pharmacy - Pharmacy Assistant</div><div>Logistics & Shelves Reorganisation</div>
                                 </div>
                                 <div className="rounded-xl border-2 m-2 mr-5 p-6">
-                                    <div className="font-bold mb-1">2019</div><div>Office National d&#39;Études et de Recherches Aérospatiales - Stage</div>
+                                    <div className="font-bold mb-1">2019</div><div>ONERA (The French Aerospace Lab) - Internship</div>
                                 </div>
                             </div>
 
--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="flex text-6xl row-start-3 justify-center">rherbin.github.io</div>
         <div className="text-2xl flex row-start-4 justify-center">
           <Link href="/portfolio" className="mx-5 px-10 py-5 hover:bg-gray-900 duration-700 rounded-xl">portfolio</Link>
-          <Link href="/cv" className="mx-5 px-10 py-5 hover:bg-gray-900 duration-700 rounded-xl">resume</Link>
+          <Link href="/cven" className="mx-5 px-10 py-5 hover:bg-gray-900 duration-700 rounded-xl">resume</Link>
           <Link href="/contact" className="mx-5 px-10 py-5 hover:bg-gray-900 duration-700 rounded-xl">contact</Link>
         </div>
       </div>
